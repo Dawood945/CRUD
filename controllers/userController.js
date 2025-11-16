12042039a10b8f09a1580730f8cb8f537f886df1
Dawood945 <dawood.mehmood945@gmail.com>
2025-11-17 00:34:57 +0500

    Initial commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,17 +4,56 @@ export const createUser = async(req, res) => {
    try {
     const userCreate = await User.create(req.body)
     userCreate.save();
-    res.json(userCreate)
+    res.status(201).json(userCreate)
    } catch (error) {
-    res.json({error: error.Message})
+    res.status(500).json({error: error.Message})
    }
 }
 
 export const getAllUser = async(req, res) => {
     try {
         const getUser = await User.find()
-        res.json(getUser)
+        res.status(200).json(getUser)
     } catch (error) {
-        res.json({error: error.Message})
+        res.status(500).json({error: error.Message})
+    }
+}
+
+export const patchUser = async(req,res) => {
+    const id = req.params.id;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, {$set: req.body}, {new:true})
+        if(!updatedUser){
+            return res.status(404).json({message:"User Not Found"})
+        }
+        res.status(200).json({message:"User Partially Updated Successfully", updatedUser})
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error",error})
+    }
+}
+
+export const updateUser = async(req,res) => {
+    const id = req.params.id;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, req.body, {new:true})
+        if(!updatedUser){
+            return res.status(404).json({message:"User Not Found"})
+        }
+        res.status(200).json({message:"User Updated Successfully", updatedUser})
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error", error})
+    }
+}
+
+export const deleteUser = async(req, res) => {
+    const id = req.params.id;
+    try {
+        const deletedUser = await User.findByIdAndDelete(id)
+        if(!deletedUser){
+          return res.status(404).json({message:"User Not Found"})
+        }
+        res.status(200).json({message:"User Deleted Successfully", deletedUser} )
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error", error})
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { createUser, getAllUser } from "../controllers/userController.js";
+import { createUser, getAllUser, patchUser, updateUser, deleteUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post('/create', createUser)
 router.get('/getAll', getAllUser)
+router.patch('/patch/:id', patchUser)
+router.put('/update/:id', updateUser)
+router.delete('/delete/:id', deleteUser)
 
 export default router;
